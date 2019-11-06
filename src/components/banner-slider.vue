03297dbf4d21746989c0fbbf1a3b5c8db876e87f
liuman <2192414704@qq.com>
2019-11-06 20:17:00 +0800

    代码提交

---
+++ src/components/banner-slider.vue
@@ -0,0 +1,96 @@
+<template>
+  <!--<div class="banner-slider">-->
+    <!--https://vuejsexamples.com/a-customizable-carousel-slider-optimized-for-vue/-->
+    <!--<hooper :progress="true" :autoPlay="true" :playSpeed="2000">-->
+    <!--<hooper>-->
+      <!--<slide>-->
+        <!--<div class="banner-slider-item item-one">-->
+          <!--<div class="item-caption">虹川科技，绿色智慧城市理念倡行者</div>-->
+          <!--<div class="item-tips">-->
+            <!--HCTTOP TECHNOLOGY<br />ADVOCATE THE CONCEPT OF GREEN SMART CITY-->
+          <!--</div>-->
+        <!--</div>-->
+      <!--</slide>-->
+      <!--<slide>-->
+        <!--<div class="banner-slider-item item-one">-->
+          <!--<div class="item-caption">虹川科技，绿色智慧城市理念倡行者</div>-->
+          <!--<div class="item-tips">-->
+            <!--HCTTOP TECHNOLOGY<br />ADVOCATE THE CONCEPT OF GREEN SMART CITY-->
+          <!--</div>-->
+        <!--</div>-->
+      <!--</slide>-->
+      <!--<hooper-pagination slot="hooper-addons"></hooper-pagination>-->
+    <!--</hooper>-->
+  <!--</div>-->
+    <div></div>
+</template>
+
+<script>
+// import { Hooper, Slide, Pagination as HooperPagination } from "hooper";
+export default {
+  name: "banner-slider",
+  components: {
+    // Hooper,
+    // Slide,
+    // HooperPagination
+  }
+};
+</script>
+
+<style lang="less" scoped>
+@import "../assets/css/global";
+/*@import "../assets/css/hooper-slider.less";*/
+
+.banner-slider {
+  width: 100%;
+  height: 480px;
+  background: linear-gradient(
+    135deg,
+    rgba(226, 242, 249, 1) 0%,
+    rgba(224, 235, 249, 1) 49%,
+    rgba(237, 234, 249, 1) 100%
+  );
+  .banner-slider-item {
+    width: 100%;
+    height: 100%;
+    .item-caption {
+      color: @font-color-active;
+      font-size: 56px;
+      padding: 169px 0 30px;
+    }
+    .item-tips {
+      color: @font-color-active;
+      font-size: 20px;
+      line-height: 28px;
+    }
+  }
+  .item-one {
+    background: url("../assets/images/img_index_banner_1_bg.png") center
+      no-repeat;
+    background-size: auto 100%;
+    text-align: center;
+  }
+
+  .two {
+    background: blue;
+  }
+}
+
+.hooper {
+  height: 100%;
+}
+.hooper-indicators {
+  background: red;
+}
+.hooper-indicator {
+  height: 12px;
+  width: 12px;
+  border-radius: 100%;
+  margin: 0 6px;
+  background: rgba(0, 0, 0, 0.2);
+}
+.hooper-indicator:hover,
+.hooper-indicator.is-active {
+  background-color: #fff;
+}
+</style>
---
+++ src/components/banner.vue
@@ -0,0 +1,90 @@
+<template>
+  <!--<div class="banner-slider">-->
+    <!--<b-carousel id="carousel-fade" fade indicators interval="1000">-->
+      <!--<b-carousel-slide caption="First slide">-->
+        <!--<div class="banner-slider-item item-one">-->
+          <!--<div class="item-caption">虹川科技，绿色智慧城市理念倡行者</div>-->
+          <!--<div class="item-tips">-->
+            <!--HCTTOP TECHNOLOGY<br />ADVOCATE THE CONCEPT OF GREEN SMART CITY-->
+          <!--</div>-->
+        <!--</div>-->
+      <!--</b-carousel-slide>-->
+      <!--<b-carousel-slide caption="Second slide">-->
+        <!--<div class="banner-slider-item item-one">-->
+          <!--<div class="item-caption">虹川科技，绿色智慧城市理念倡行者</div>-->
+          <!--<div class="item-tips">-->
+            <!--HCTTOP TECHNOLOGY<br />ADVOCATE THE CONCEPT OF GREEN SMART CITY-->
+          <!--</div>-->
+        <!--</div>-->
+      <!--</b-carousel-slide>-->
+    <!--</b-carousel>-->
+  <!--</div>-->
+
+    <b-carousel
+            id="carousel-fade"
+            style="text-shadow: 0px 0px 2px #000"
+            fade
+            indicators
+            img-width="1024"
+            img-height="480"
+            interval="1000"
+    >
+        <b-carousel-slide
+                caption="First slide"
+                img-src="https://picsum.photos/1024/480/?image=10"
+        ></b-carousel-slide>
+        <b-carousel-slide
+                caption="Second Slide"
+                img-src="https://picsum.photos/1024/480/?image=12"
+        ></b-carousel-slide>
+        <b-carousel-slide
+                caption="Third Slide"
+                img-src="https://picsum.photos/1024/480/?image=22"
+        ></b-carousel-slide>
+    </b-carousel>
+</template>
+
+<script>
+export default {
+  name: "banner"
+};
+</script>
+
+<style lang="less" scoped>
+@import "../assets/css/global";
+
+.banner-slider {
+  width: 100%;
+  height: 480px;
+  background: linear-gradient(
+    135deg,
+    rgba(226, 242, 249, 1) 0%,
+    rgba(224, 235, 249, 1) 49%,
+    rgba(237, 234, 249, 1) 100%
+  );
+  .banner-slider-item {
+    width: 100%;
+    height: 100%;
+    .item-caption {
+      color: @font-color-active;
+      font-size: 56px;
+      padding: 169px 0 30px;
+    }
+    .item-tips {
+      color: @font-color-active;
+      font-size: 20px;
+      line-height: 28px;
+    }
+  }
+  .item-one {
+    background: url("../assets/images/img_index_banner_1_bg.png") center
+      no-repeat;
+    background-size: auto 100%;
+    text-align: center;
+  }
+
+  .two {
+    background: blue;
+  }
+}
+</style>
