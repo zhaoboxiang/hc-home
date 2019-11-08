9f4b9cb068ccce32ed65d9413f993a4cd93ffd75
zhaoboxiang <zbx7858@gmail.com>
2019-11-08 15:08:11 +0800

    子路由 菜单 组件

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,19 +6,69 @@
       /></router-link>
     </div>
     <div class="hc-nav">
-      <router-link :to="{ name: 'home' }">首页</router-link>
-      <router-link :to="{ name: 'about' }">关于虹川</router-link>
-      <router-link :to="{ name: 'solution' }">解决方案</router-link>
-      <router-link :to="{ name: 'product' }">产品中心</router-link>
-      <router-link :to="{ name: 'careers' }">广纳人才</router-link>
+      <div class="nav-link-wrap">
+        <router-link exact :to="{ name: 'home' }">首页</router-link>
+      </div>
+      <div class="nav-link-wrap">
+        <router-link :to="{ name: 'about' }">关于虹川</router-link>
+      </div>
+      <div
+        class="nav-link-wrap"
+        @mouseenter="showSubNav"
+        @mouseleave="hideSubNav"
+      >
+        <router-link :to="{ name: 'solution' }">解决方案</router-link>
+        <drop-down-menu
+          v-show="hasSubNav"
+          :menu-items="solutionMenuItems"
+          @hideMenu="hideMenu"
+        ></drop-down-menu>
+      </div>
+      <div class="nav-link-wrap">
+        <router-link :to="{ name: 'product' }">产品中心</router-link>
+      </div>
+      <div class="nav-link-wrap">
+        <router-link :to="{ name: 'careers' }">广纳人才</router-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import DropDownMenu from "./DropDownMenu";
 export default {
-  name: "Header"
+  name: "Header",
+  data() {
+    return {
+      hasSubNav: false,
+      solutionMenuItems: [
+        { name: "wisdom-city", title: "智慧城市" },
+        { name: "wisdom-environment", title: "智慧环保" },
+        { name: "wisdom-water", title: "智慧水务" },
+        { name: "wisdom-park", title: "智慧园区" }
+      ]
+    };
+  },
+  components: {
+    DropDownMenu
+  },
+  methods: {
+    showSubNav() {
+      this.hasSubNav = true;
+    },
+    hideSubNav() {
+      this.hasSubNav = false;
+    },
+    hideMenu(value) {
+      console.log(value)
+      this.hasSubNav = value
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.nav-link-wrap {
+  display: inline-block;
+}
+</style>
--- src/views/Solution.vue
+++ src/views/Solution.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="solution">
-    <hc-header></hc-header>
     <!--智慧城市-->
     <div hidden>
       <div class="solution-banner-box">
@@ -9,9 +8,9 @@
             <div class="ban-h3">智慧城市</div>
             <div class="ban-h6">信息技术与城市现代化深度融合、迭代演进</div>
             <div class="ban-p">
-              智慧城市定位为城市内生态环境、公共管理、公共服务的决策指挥大脑，<br/>
-              以“物联、数联、智联”为特征，做强核心、广泛延伸、细化感知，创新城市日常管理和运营模式的手段。<br/>
-              强化主动发现和协同指挥处置功能，借助人工智能、大数据、物联网新技术，实现对城市精细化管理的智能化手段，<br/>
+              智慧城市定位为城市内生态环境、公共管理、公共服务的决策指挥大脑，<br />
+              以“物联、数联、智联”为特征，做强核心、广泛延伸、细化感知，创新城市日常管理和运营模式的手段。<br />
+              强化主动发现和协同指挥处置功能，借助人工智能、大数据、物联网新技术，实现对城市精细化管理的智能化手段，<br />
               不断提升城市在环保、水利水务、城市管理等方面复杂事项的多部门协同指挥处置。
             </div>
           </div>
@@ -26,20 +25,33 @@
             <div class="flex-item">
               <div class="item-icon icon-1"></div>
               <div class="item-tips">人·幸福</div>
-              <div class="item-h">民生及管理智慧化<br/>社会治理 · 人文智慧</div>
-              <div class="item-p">促进民生服务和社会管理智慧化<br/>智慧教育、智慧医疗、智慧政务<br/>…</div>
+              <div class="item-h">
+                民生及管理智慧化<br />社会治理 · 人文智慧
+              </div>
+              <div class="item-p">
+                促进民生服务和社会管理智慧化<br />智慧教育、智慧医疗、智慧政务<br />…
+              </div>
             </div>
             <div class="flex-item">
               <div class="item-icon icon-2"></div>
               <div class="item-tips">城·智能</div>
-              <div class="item-h">基础设施智慧化<br/> 基础设施 · 物联智慧</div>
-              <div class="item-p">建设智慧化的城市基础设施<br/> 智慧楼宇、智慧照明、智慧交通、智慧能源<br/>…</div>
+              <div class="item-h">
+                基础设施智慧化<br />
+                基础设施 · 物联智慧
+              </div>
+              <div class="item-p">
+                建设智慧化的城市基础设施<br />
+                智慧楼宇、智慧照明、智慧交通、智慧能源<br />…
+              </div>
             </div>
             <div class="flex-item">
               <div class="item-icon icon-3"></div>
               <div class="item-tips">市·繁华</div>
-              <div class="item-h">经济产业智慧化<br/>经济生产 · 产业智慧</div>
-              <div class="item-p">促进工农业生产、市场经济智慧化<br/> 智慧产业、智慧农业、电子商务<br/>…</div>
+              <div class="item-h">经济产业智慧化<br />经济生产 · 产业智慧</div>
+              <div class="item-p">
+                促进工农业生产、市场经济智慧化<br />
+                智慧产业、智慧农业、电子商务<br />…
+              </div>
             </div>
           </div>
         </div>
@@ -48,7 +60,9 @@
         <div class="content section-content">
           <h3 class="section-h3">顶层架构</h3>
           <h6 class="section-h6">TOP ARCHITECTURE</h6>
-          <div class="info-container"><img src="../assets/images/solution/img_jjfa_zhcs_dcjg.png"/></div>
+          <div class="info-container">
+            <img src="../assets/images/solution/img_jjfa_zhcs_dcjg.png" />
+          </div>
           <div class="tec-box">
             <div class="tec-item tec-item-1">
               <div class="tips-en">BIG DATA</div>
@@ -72,22 +86,26 @@
           <div class="section-flex">
             <div class="flex-item item-1">
               <div class="left">1</div>
-              <div class="right">从顶层规划的理念出发，构建符合城市实际和特色的一整套智慧城市的综合信息服务体系。</div>
+              <div class="right">
+                从顶层规划的理念出发，构建符合城市实际和特色的一整套智慧城市的综合信息服务体系。
+              </div>
             </div>
             <div class="flex-item item-2">
               <div class="left">2</div>
-              <div class="right">构建脸大支撑平台，能够支撑智慧城市建设的城市云计算平台和智慧城市大数据融合服务平台。</div>
+              <div class="right">
+                构建脸大支撑平台，能够支撑智慧城市建设的城市云计算平台和智慧城市大数据融合服务平台。
+              </div>
             </div>
             <div class="flex-item item-n">
               <div class="left">N</div>
-              <div class="right">在智慧城市云计算中心的基础上，搭建包括城市运行体征、民生民情、生态环境、水文水利等多项应用体系，形成智慧城市的完整生态系统。</div>
+              <div class="right">
+                在智慧城市云计算中心的基础上，搭建包括城市运行体征、民生民情、生态环境、水文水利等多项应用体系，形成智慧城市的完整生态系统。
+              </div>
             </div>
-
           </div>
         </div>
       </div>
     </div>
-
 
     <!--智慧环保-->
     <div hidden>
@@ -97,9 +115,9 @@
             <div class="ban-h3">智慧环保</div>
             <div class="ban-h6">覆盖生态环境全数据，为中国生态保驾护航</div>
             <div class="ban-p">
-              环保产业发展事关国计民生，是我国重要的战略性产业。<br/>
-              虹川科技智慧环保解决方案旨在打造集环境感知、分析、管控、治理于一体的全产业链创新综合体，<br/>
-              整合联合环保产业链的创新资源，综合运用环境物联网、人工智能、高密度、全覆盖监测和大数据分析等技术手段，<br/>
+              环保产业发展事关国计民生，是我国重要的战略性产业。<br />
+              虹川科技智慧环保解决方案旨在打造集环境感知、分析、管控、治理于一体的全产业链创新综合体，<br />
+              整合联合环保产业链的创新资源，综合运用环境物联网、人工智能、高密度、全覆盖监测和大数据分析等技术手段，<br />
               以源解析加减排规划制定为科技指引，结合大数据应用分析，精准找源，科学治理，实现区域环境持续达标治理与保持经济发展平衡。
             </div>
           </div>
@@ -136,9 +154,10 @@
           <h6 class="section-h6">TOP ARCHITECTURE</h6>
           <div class="info-container">
             <div class="info-title">
-              <span>1</span>个数据中心<span>3</span>大保障体系<span>5</span>层技术支撑<span>N</span>个应用服务
+              <span>1</span
+              >个数据中心<span>3</span>大保障体系<span>5</span>层技术支撑<span>N</span>个应用服务
             </div>
-            <img src="../assets/images/solution/img_jjfa_zhhb_dcjg.png"/>
+            <img src="../assets/images/solution/img_jjfa_zhhb_dcjg.png" />
           </div>
         </div>
       </div>
@@ -175,7 +194,6 @@
                 搭建公众等多元主体共同参与到环境治理中，推动构建多主体联动，多对象共治、多方式优化的多元共治环保格局。
               </div>
             </div>
-
           </div>
         </div>
       </div>
@@ -186,12 +204,14 @@
       <div class="content banner-content">
         <div class="ban-left">
           <div class="ban-h3">智慧水务</div>
-          <div class="ban-h6">供水排水水务全流程管理，为城市生活供给生命源泉</div>
+          <div class="ban-h6">
+            供水排水水务全流程管理，为城市生活供给生命源泉
+          </div>
           <div class="ban-p">
-            水是生命之源，水务是国民经济的命脉。虹川科技智慧水务方案全面应用数字化综合信息新技术手段，<br/>
-            改变传统水务设备分散、操作复杂等状况，大幅度提高设备利用率，降低现场工作人员劳动强度，保障用水安全。<br/>
-            智慧水务创新性的“一张图”、“一个中心数据库”的建设方案，实现对信息数据统一管理和二三维应用展示，<br/>
-            将物联网和大数据技术应用于水源地、自来水厂、城市供水管网的供水及企业、居民排水、污水处理厂、废水排放的排水整体水循环全过程监控，<br/>
+            水是生命之源，水务是国民经济的命脉。虹川科技智慧水务方案全面应用数字化综合信息新技术手段，<br />
+            改变传统水务设备分散、操作复杂等状况，大幅度提高设备利用率，降低现场工作人员劳动强度，保障用水安全。<br />
+            智慧水务创新性的“一张图”、“一个中心数据库”的建设方案，实现对信息数据统一管理和二三维应用展示，<br />
+            将物联网和大数据技术应用于水源地、自来水厂、城市供水管网的供水及企业、居民排水、污水处理厂、废水排放的排水整体水循环全过程监控，<br />
             对超过警戒值提供告警提示；提供移动巡检能力，及时、快速、高效进行事故应对和指挥调度。
           </div>
         </div>
@@ -241,7 +261,7 @@
               <div class="course-tips">目的成效</div>
             </div>
           </div>
-          <img src="../assets/images/solution/img_jjfa_zhsw_dcjg.png"/>
+          <img src="../assets/images/solution/img_jjfa_zhsw_dcjg.png" />
         </div>
       </div>
     </div>
@@ -252,15 +272,21 @@
         <div class="section-flex">
           <div class="flex-item">
             <div class="item-t i-t-1">开源节流</div>
-            <div class="item-p">从水源地到水龙头，从水龙头到自然环境，整个流程保障水质的安全性，实时掌控水质变化情况，及时发现水质问题，问题发生点，并进行问题追踪，充分保障居民用水安全。</div>
+            <div class="item-p">
+              从水源地到水龙头，从水龙头到自然环境，整个流程保障水质的安全性，实时掌控水质变化情况，及时发现水质问题，问题发生点，并进行问题追踪，充分保障居民用水安全。
+            </div>
           </div>
           <div class="flex-item">
             <div class="item-t i-t-2">产销智控</div>
-            <div class="item-p">从供水角度，达到生产工艺过程自动且可控。从用水角度，做到实时掌控用水情况，并可对用水情况进行预测，从而做到供水和用水的平衡化和智能化。</div>
+            <div class="item-p">
+              从供水角度，达到生产工艺过程自动且可控。从用水角度，做到实时掌控用水情况，并可对用水情况进行预测，从而做到供水和用水的平衡化和智能化。
+            </div>
           </div>
           <div class="flex-item">
             <div class="item-t i-t-3">数治全程</div>
-            <div class="item-p">充分调用各类水务相关数据，实现从水源地—输水管网—水厂—供水站—用户—排水管—污水处理厂—排水口全过程数据收集融合应用。</div>
+            <div class="item-p">
+              充分调用各类水务相关数据，实现从水源地—输水管网—水厂—供水站—用户—排水管—污水处理厂—排水口全过程数据收集融合应用。
+            </div>
           </div>
         </div>
       </div>
@@ -268,32 +294,26 @@
     <!--智慧园区-->
 
     <!--<div class="section section-bak">-->
-      <!--<div class="content section-content">-->
-        <!--<h3 class="section-h3">顶层架构</h3>-->
-        <!--<h6 class="section-h6">TOP ARCHITECTURE</h6>-->
-        <!--<div class="info-container section-flex">-->
-          <!--<div class="flex-item"></div>-->
-          <!--<div class="flex-item"></div>-->
-          <!--<div class="flex-item"></div>-->
-        <!--</div>-->
-      <!--</div>-->
+    <!--<div class="content section-content">-->
+    <!--<h3 class="section-h3">顶层架构</h3>-->
+    <!--<h6 class="section-h6">TOP ARCHITECTURE</h6>-->
+    <!--<div class="info-container section-flex">-->
+    <!--<div class="flex-item"></div>-->
+    <!--<div class="flex-item"></div>-->
+    <!--<div class="flex-item"></div>-->
+    <!--</div>-->
+    <!--</div>-->
     <!--</div>-->
     <hc-footer></hc-footer>
   </div>
 </template>
 
 <script>
-  import HcHeader from "../components/hc-header";
-  import HcFooter from "../components/hc-footer";
 export default {
-  name: "solution",
-  components: {
-    HcHeader,
-    HcFooter
-  },
+  name: "solution"
 };
 </script>
 
 <style lang="less" scoped>
-  @import "../assets/css/solution";
+@import "../assets/css/solution";
 </style>
