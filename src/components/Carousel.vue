54b61e10b2539a7bc98f637b13d56887c098663e
liuman <2192414704@qq.com>
2019-11-11 15:57:55 +0800

    首页banner样式优化

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -3,8 +3,26 @@
     <md-card-media>
       <!-- swiper -->
       <swiper :options="swiperOption">
-        <swiper-slide class="slide-1"></swiper-slide>
-        <swiper-slide class="slide-2"></swiper-slide>
+        <swiper-slide class="slide-1">
+          <div class="slide-content">
+            <div class="tips">
+              <div class="tips-ch">虹川科技，绿色智慧城市理念倡行者</div>
+              <div class="tips-en">
+                HCTTOP TECHNOLOGY<br />GREEN SMART CITY COMMEMORATE ADVOCATE
+              </div>
+            </div>
+          </div>
+        </swiper-slide>
+        <!--<swiper-slide class="slide-2">-->
+          <!--<div class="slide-content">-->
+            <!--<div class="tips">-->
+              <!--<div class="tips-ch">111虹川科技，绿色智慧城市理念倡行者</div>-->
+              <!--<div class="tips-en">-->
+                <!--111HCTTOP TECHNOLOGY<br />111GREEN SMART CITY COMMEMORATE ADVOCATE-->
+              <!--</div>-->
+            <!--</div>-->
+          <!--</div>-->
+        <!--</swiper-slide>-->
         <div
           class="swiper-pagination swiper-pagination-white"
           slot="pagination"
@@ -39,22 +57,45 @@ export default {
 <style lang="scss" scoped>
 .swiper-slide {
   height: 480px;
-  background-size: cover;
-  background: #e2f2f9
-      linear-gradient(
-        135deg,
-        rgba(226, 242, 249, 1) 0%,
-        rgba(224, 235, 249, 1) 49%,
-        rgba(237, 234, 249, 1) 100%
-      ),
-    no-repeat, center;
+  .slide-content {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    margin: 0 auto;
+    font-family: PingFangSC-Semibold, PingFang SC;
+    .tips {
+      color: #303ca8;
 
-  &.slide-1 {
-    background-image: url("../assets/images/banner_0191107113737.png");
+      .tips-ch {
+        font-size: 56px;
+        font-weight: bold;
+      }
+      .tips-en {
+        font-size: 24px;
+        margin-top: 24px;
+      }
+    }
   }
 
-  &.slide-2 {
-    /*background-image: url("../assets/images/img_about_company_photo.png");*/
+  &.slide-1{
+    background: linear-gradient(to right, #e2f1f9, #eceaf9);
+    .slide-content {
+      background: url("../assets/images/img_index_banner_1_bg.png") center
+      no-repeat;
+      background-size: auto 100%;
+    }
+  }
+
+  &.slide-2{
+    background: #eceaf9;
+    .slide-content {
+      background: url("../assets/images/img_index_banner_1_bg.png") center
+      no-repeat;
+      background-size: auto 100%;
+    }
   }
 }
 </style>
