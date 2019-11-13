3a0e12391a281c6db24610aca61f1738ee90f10e
zhaoboxiang <zbx7858@gmail.com>
2019-11-13 10:56:18 +0800

    change router 'history' mode to 'hash' mode

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -1,13 +1,22 @@
 <template>
-    $END$
+    <div class="hc-not-found">
+        <h1>The Page You Looking for Not Found.</h1>
+    </div>
+
 </template>
 
 <script>
-    export default {
-        name: "NotFoundPage"
-    }
+export default {
+  name: "NotFoundPage"
+};
 </script>
 
 <style scoped>
-
+.hc-not-found {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 32px;
+  font-weight: bold;
+}
 </style>
